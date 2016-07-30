691bca6479bf3302151f184a321c810e158fa0db
sdiazmihell <Sebastian@Sebastians-MacBook-Pro.local>
2016-07-30 14:25:40 -0700

    Getting smooth scroll working and other small cleanup stuff

--- final.js
+++ final.js
@@ -16,49 +16,16 @@ var startup = function () {
 	 button.text(butText);
 	});
 
-  $("a.backToTop").on("click", function () {
-  	$("#pageTop")[0].scrollIntoView({
-  		behavior: "smooth"
-  	});
-  	return false;
-  });	
-
-  $("a#aboutLink").on("click", function () {
-  	$("#aboutSection")[0].scrollIntoView({
-  		behavior: "smooth"
-  	});
-  	return false;
-  });
-
-  $("a#portfolioLink").on("click", function () {
-  	$("#portfolioSection")[0].scrollIntoView({
-  		behavior: "smooth"
-  	});
-  	return false;
-  });
-
-  $("a#contactLink").on("click", function () {
-  	$("#contactSection")[0].scrollIntoView({
-  		behavior: "smooth"
-  	});
-  	return false;
-  });
-
-  $("a#otherLink").on("click", function () {
-  	$("#otherSection")[0].scrollIntoView({
-  		behavior: "smooth"
-  	});
-  	return false;
+  $("a[href^='#']").on("click", function () {
+    var href = $(this).attr("href");
+    var targetElem = $(href);
+    targetElem[0].scrollIntoView({
+      behavior: "smooth"
+    });
+    return false;
   });
 
 };
-
-
-
-
-
-
-
 
 
 $(document).ready(startup);
